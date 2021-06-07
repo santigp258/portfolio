6d1fd7fc8ff3aa85acab24061ae6c7d301bfa145
Santigp258 <santigp258@gmail.com>
2021-06-07 15:53:11 -0500

    hero mobile resolution

--- app/src/pages/landing/main/Hero.tsx
+++ app/src/pages/landing/main/Hero.tsx
@@ -3,14 +3,14 @@ import Typed from 'react-typed';
 export const Hero: React.FC = () => {
   return (
     <section className="px-6 md:w-9/12 md:m-auto md:px-0">
-      <div className="hero__content-left">
-        <h3>
+      <div>
+        <h3 className="text-2xl">
           <Typed strings={['Hola, soy']} typeSpeed={50} showCursor={false} />
         </h3>
-        <h1>
+        <h1 className="text-4xl text-primary font-bold">
           <Typed strings={['Santiago']} typeSpeed={150} showCursor={false} />
         </h1>
-        <h3>
+        <h3 className="text-2xl font-semibold mb-2">
           <Typed
             strings={[
               'Me gusta programar',
@@ -21,27 +21,30 @@ export const Hero: React.FC = () => {
             loop
           />
         </h3>
-        <p>
+        <p className="text-justify">
           Soy desarrollador
-          <span style={{ fontWeight: 'bold' }}> Front End</span>, vivo en Colombia.
-          Entusiasta del mundo del software con un gran enfoque en el desarrollo web.
-          Siempre encontrando la mejor forma de hacer las cosas, aprendiendo cada día y
-          dando lo mejor de mí en cada proyecto.
+          <span className="font-semibold"> Front End</span>, vivo en Colombia. Entusiasta
+          del mundo del software con un gran enfoque en el desarrollo web. Siempre
+          encontrando la mejor forma de hacer las cosas, aprendiendo cada día y dando lo
+          mejor de mí en cada proyecto.
         </p>
-        <a href="#contact">
-          <button className="portfolio_button hero__button-talk" type="button">
+        <button
+          className="btn btn-blue m-auto block w-full my-3 uppercase shadow"
+          type="button"
+        >
+          <a href="#contact" className="font-semibold">
             Contáctame
-          </button>
-        </a>
-        <a href="#works">
-          <button className="portfolio_button hero__button-works" type="button">
+          </a>
+        </button>
+        <button className="btn m-auto block w-full my-3 uppercase" type="button">
+          <a href="#works" className="font-semibold">
             Mis trabajos
-          </button>
-        </a>
+          </a>
+        </button>
       </div>
-      <div className="hero__content-right">
-        <h2 className="hero__text">Developer</h2>
-        <img src="./img/hero.png" className="hero__img" alt="developer" />
+      <div className="hidden md:block">
+        <h2 className="">Developer</h2>
+        <img src="./img/hero.png" alt="developer" />
       </div>
     </section>
   );
